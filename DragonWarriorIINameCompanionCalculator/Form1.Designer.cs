e296f3a246b16248e495ea1e7afd41b5355f7a16
Shawn M. Crawford <shawn.crawford@crown.com>
2019-10-17 16:58:18 -0400

    Initial Commit

---
+++ DragonWarriorIINameCompanionCalculator/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace DragonWarriorIINameCompanionCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxPrinceOfCannock = new System.Windows.Forms.TextBox();
+            this.textBoxPrincessOfMoonbrooke = new System.Windows.Forms.TextBox();
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonGenerate);
+            this.groupBox1.Controls.Add(this.textBoxName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(326, 41);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(351, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "&About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hero name:";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(137, 13);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBoxPrincessOfMoonbrooke);
+            this.groupBox2.Controls.Add(this.textBoxPrinceOfCannock);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 74);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(326, 80);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Prince of Cannock:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Princess of Moonbrooke:";
+            // 
+            // textBoxPrinceOfCannock
+            // 
+            this.textBoxPrinceOfCannock.Location = new System.Drawing.Point(137, 19);
+            this.textBoxPrinceOfCannock.Name = "textBoxPrinceOfCannock";
+            this.textBoxPrinceOfCannock.ReadOnly = true;
+            this.textBoxPrinceOfCannock.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPrinceOfCannock.TabIndex = 2;
+            // 
+            // textBoxPrincessOfMoonbrooke
+            // 
+            this.textBoxPrincessOfMoonbrooke.Location = new System.Drawing.Point(137, 45);
+            this.textBoxPrincessOfMoonbrooke.Name = "textBoxPrincessOfMoonbrooke";
+            this.textBoxPrincessOfMoonbrooke.ReadOnly = true;
+            this.textBoxPrincessOfMoonbrooke.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPrincessOfMoonbrooke.TabIndex = 3;
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Location = new System.Drawing.Point(243, 11);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(75, 23);
+            this.buttonGenerate.TabIndex = 2;
+            this.buttonGenerate.Text = "&Generate";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Dragon Warrior II Name Companion Calc";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBoxPrincessOfMoonbrooke;
+        private System.Windows.Forms.TextBox textBoxPrinceOfCannock;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ DragonWarriorIINameCompanionCalculator/Form1.cs
@@ -0,0 +1,365 @@
+﻿/**
+ *  @file           DragonWarriorIINameCompanionCalculator.cs
+ *  @brief          Defines the logic for calculating the companion names from the entered heroes name.
+ *  
+ *  @copyright      2019 Shawn M. Crawford [sleepy]
+ *  @date           10/17/2019
+ *
+ *  @remark Author  Shawn M. Crawford [sleepy]
+ *
+ *  @note           N/A
+ *
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DragonWarriorIINameCompanionCalculator
+{
+    public partial class Form1 : Form
+    {
+
+        private static readonly string[] _PRINCE_NAMES =
+            { "Bran", "Glynn", "Talint", "Numor", "Lars", "Orfeo", "Artho", "Esgar" };
+
+        private static readonly string[] _PRINCESS_NAMES =
+            { "Varia", "Elani", "Ollisa", "Roz", "Kailin", "Peta", "Illyth", "Gwen" };
+
+        private static readonly int _MAGIC_NUMBER_1 = 0x180;
+
+        private static readonly int _MAGIC_NUMBER_2 = 0x7;
+
+        private static readonly int _MAGIC_NUMBER_3 = 0x38;
+
+        private static readonly int _MAGIC_NUMBER_4 = 0x8;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGenerate_Click(object sender, EventArgs e)
+        {
+            string heroName = textBoxName.Text;
+            string hexValue;
+            string hexValue2;
+            string hexValue3;
+            string hexValue4;
+            string totalHexValue;
+            string tempHexValue;
+            int value;
+            int value2;
+            int value3;
+            int value4;
+            int totalValue;
+            int tempValue;
+            int finalValue1;
+            int finalValue2;
+
+            if (heroName.Length != 0)
+            {
+                hexValue = GetCharHexValue(heroName[0]);
+
+                if (heroName.Length >= 2)
+                {
+                    hexValue2 = GetCharHexValue(heroName[1]);
+                }
+                else
+                {
+                    hexValue2 = "00";
+                }
+
+                if (heroName.Length >= 3)
+                {
+                    hexValue3 = GetCharHexValue(heroName[2]);
+                }
+                else
+                {
+                    hexValue3 = "00";
+                }
+
+                if (heroName.Length >= 4)
+                {
+                    hexValue4 = GetCharHexValue(heroName[3]);
+                }
+                else
+                {
+                    hexValue4 = "00";
+                }
+
+
+                if (hexValue == "??" || hexValue2 == "??" || hexValue3 == "??" || hexValue4 == "??")
+                {
+                    MessageBox.Show(@"Invalid character in name.", @"Name Companion Calc", MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+
+                    
+                }
+                else
+                {
+                    value = int.Parse(hexValue, System.Globalization.NumberStyles.HexNumber);
+                    value2 = int.Parse(hexValue2, System.Globalization.NumberStyles.HexNumber);
+                    value3 = int.Parse(hexValue3, System.Globalization.NumberStyles.HexNumber);
+                    value4 = int.Parse(hexValue4, System.Globalization.NumberStyles.HexNumber);
+
+                    totalValue = value + value2 + value3 + value4 + _MAGIC_NUMBER_1;
+                    totalHexValue = totalValue.ToString("X");
+
+                    tempValue = int.Parse(totalHexValue[0].ToString(), System.Globalization.NumberStyles.HexNumber);
+                    totalValue = totalValue + tempValue;
+
+                    tempHexValue = totalHexValue[0].ToString() + "00";
+                    tempValue = int.Parse(tempHexValue, System.Globalization.NumberStyles.HexNumber);
+                    totalValue = totalValue - tempValue;
+
+                    // Prince
+                    finalValue1 = totalValue & _MAGIC_NUMBER_2;
+
+                    totalValue = totalValue & _MAGIC_NUMBER_3;
+
+                    // Princess
+                    finalValue2 = totalValue / _MAGIC_NUMBER_4;
+
+                    if (finalValue1 >= 0 && finalValue1 <= 7)
+                    {
+                        textBoxPrinceOfCannock.Text = _PRINCE_NAMES[finalValue1];
+                    }
+                    else
+                    {
+                        textBoxPrinceOfCannock.Text = @"Error";
+                    }
+
+                    if (finalValue2 >= 0 && finalValue2 <= 7)
+                    {
+                        textBoxPrincessOfMoonbrooke.Text = _PRINCESS_NAMES[finalValue2];
+                    }
+                    else
+                    {
+                        textBoxPrincessOfMoonbrooke.Text = @"Error";
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show(@"Please enter a name for the hero.", @"Name Companion Calc", MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+
+            
+
+        }
+
+        private string GetCharHexValue(char c)
+        {
+            string hexValue;
+
+            switch (c)
+            {
+                case 'A':
+                    hexValue = "24";
+                    break;
+                case 'B':
+                    hexValue = "25";
+                    break;
+                case 'C':
+                    hexValue = "26";
+                    break;
+                case 'D':
+                    hexValue = "27";
+                    break;
+                case 'E':
+                    hexValue = "28";
+                    break;
+                case 'F':
+                    hexValue = "29";
+                    break;
+                case 'G':
+                    hexValue = "2A";
+                    break;
+                case 'H':
+                    hexValue = "2B";
+                    break;
+                case 'I':
+                    hexValue = "2C";
+                    break;
+                case 'J':
+                    hexValue = "2D";
+                    break;
+                case 'K':
+                    hexValue = "2E";
+                    break;
+                case 'L':
+                    hexValue = "2F";
+                    break;
+                case 'M':
+                    hexValue = "30";
+                    break;
+                case 'N':
+                    hexValue = "31";
+                    break;
+                case 'O':
+                    hexValue = "32";
+                    break;
+                case 'P':
+                    hexValue = "33";
+                    break;
+                case 'Q':
+                    hexValue = "34";
+                    break;
+                case 'R':
+                    hexValue = "35";
+                    break;
+                case 'S':
+                    hexValue = "36";
+                    break;
+                case 'T':
+                    hexValue = "37";
+                    break;
+                case 'U':
+                    hexValue = "38";
+                    break;
+                case 'V':
+                    hexValue = "39";
+                    break;
+                case 'W':
+                    hexValue = "3A";
+                    break;
+                case 'X':
+                    hexValue = "3B";
+                    break;
+                case 'Y':
+                    hexValue = "3C";
+                    break;
+                case 'Z':
+                    hexValue = "3D";
+                    break;
+                case '\'':
+                    hexValue = "5F";
+                    break;
+                case ',':
+                    hexValue = "69";
+                    break;
+                case '.':
+                    hexValue = "6B";
+                    break;
+                case ';':
+                    hexValue = "70";
+                    break;
+                case '~':
+                    hexValue = "75";
+                    break;
+                case '>':
+                    hexValue = "63";
+                    break;
+                case ' ':
+                    hexValue = "60";
+                    break;
+                case 'a':
+                    hexValue = "0A";
+                    break;
+                case 'b':
+                    hexValue = "0B";
+                    break;
+                case 'c':
+                    hexValue = "0C";
+                    break;
+                case 'd':
+                    hexValue = "0D";
+                    break;
+                case 'e':
+                    hexValue = "0E";
+                    break;
+                case 'f':
+                    hexValue = "0F";
+                    break;
+                case 'g':
+                    hexValue = "10";
+                    break;
+                case 'h':
+                    hexValue = "11";
+                    break;
+                case 'i':
+                    hexValue = "12";
+                    break;
+                case 'j':
+                    hexValue = "13";
+                    break;
+                case 'k':
+                    hexValue = "14";
+                    break;
+                case 'l':
+                    hexValue = "15";
+                    break;
+                case 'm':
+                    hexValue = "16";
+                    break;
+                case 'n':
+                    hexValue = "17";
+                    break;
+                case 'o':
+                    hexValue = "18";
+                    break;
+                case 'p':
+                    hexValue = "19";
+                    break;
+                case 'q':
+                    hexValue = "1A";
+                    break;
+                case 'r':
+                    hexValue = "1B";
+                    break;
+                case 's':
+                    hexValue = "1C";
+                    break;
+                case 't':
+                    hexValue = "1D";
+                    break;
+                case 'u':
+                    hexValue = "1E";
+                    break;
+                case 'v':
+                    hexValue = "1F";
+                    break;
+                case 'w':
+                    hexValue = "20";
+                    break;
+                case 'x':
+                    hexValue = "21";
+                    break;
+                case 'y':
+                    hexValue = "22";
+                    break;
+                case 'z':
+                    hexValue = "23";
+                    break;
+                case '!':
+                    hexValue = "6F";
+                    break;
+                case '?':
+                    hexValue = "6E";
+                    break;
+                default:
+                    hexValue = "??";
+                    break;
+            }
+
+            return hexValue;
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox aboutBox = new AboutBox();
+            aboutBox.ShowDialog();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
